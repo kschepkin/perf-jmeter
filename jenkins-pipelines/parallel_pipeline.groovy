6ba3ecc67032212309cffce5b87945514b7191a3
Konstantin <Konstantin.schepkin@gmail.com>
2023-10-07 22:26:14 +0300

    update pipeline script

--- jenkins-pipelines/parallel_pipeline.groovy
+++ jenkins-pipelines/parallel_pipeline.groovy
@@ -2,11 +2,15 @@ pipeline {
     agent any
     
     environment {
-        server1="user@127.0.0.1"
-        server2="login@192.168.1.2"
+        server1="login@127.0.0.1"
+        server2="OFF"
         server3="OFF"
         server4="OFF"
- 
+        jmeterversion="5.5"
+        project="mytestprojectname"
+        preparecommand="rm -rf ./perf && mkdir perf && mkdir perf/results && unzip perf.zip -d perf"
+        startcommand="export _JAVA_OPTIONS='-Xms3072m -Xmx6144m' && cd perf && ./apache-jmeter/bin/jmeter.sh -n -t ~/perf/scripts/${project}/${PERF_TEST}.jmx -Jthreads=${threads} -Jrampup=${rampup} -Jduration=${duration} -Jloops=${loops} -l ~/perf/results/${PERF_TEST}/${BUILD_NUMBER}.jtl"
+        killcommand="kill -9 $(ps aux | grep jmeter | grep -v \"grep\" | awk '{print $2}')"
     }
 
     stages {
@@ -15,10 +19,12 @@ pipeline {
             
             steps {
                 sh "rm -rf ./*"
-                git credentialsId: 'Credentinals-Name', url: 'https://repo.url/repo.git'
-                sh "unzip ./apache-jmeter-5.3.zip -d ./"
-                sh "chmod 0755 ./apache-jmeter-5.3/bin/jmeter.sh"
-                sh "zip -r perf.zip apache-jmeter-5.3 scripts"
+                git credentialsId: 'gitlab', url: 'https://gitlab.com/konstantin.schepkin/perf.git'
+                sh "wget https://apache-mirror.rbc.ru/pub/apache/jmeter/binaries/apache-jmeter-${jmeterversion}.zip -O ./apache-jmeter.zip"
+                sh "unzip ./apache-jmeter.zip -d ./ && mv apache-jmeter-${jmeterversion} apache-jmeter"
+                sh "cp -R ./plugins/* ./apache-jmeter/lib/ext/"
+                sh "chmod 0755 ./apache-jmeter/bin/jmeter.sh"
+                sh "zip -r perf.zip apache-jmeter scripts/${project}"
             }
         }
 
@@ -66,28 +72,28 @@ stage('Prepare loaders') {
                         "Load1": {
                             script {
                                 if (server1 != "OFF") {
-                                    sh "ssh  ${server1} \"rm -rf ./perf && mkdir perf && mkdir perf/results && unzip perf.zip -d perf \" "
+                                    sh "ssh  ${server1} \"${preparecommand}\""
                                 }
                             }
                         },
                         "Load2": {
                             script {
                                 if (server2 != "OFF") {
-                                    sh "ssh  ${server2} \"rm -rf ./perf && mkdir perf && mkdir perf/results && unzip perf.zip -d perf \" "
+                                    sh "ssh  ${server2} \"${preparecommand}\""
                                 }
                             }
                         },
                         "Load3": {
                             script {
                                 if (server3 != "OFF") {
-                                    sh "ssh  ${server3} \"rm -rf ./perf && mkdir perf && mkdir perf/results && unzip perf.zip -d perf \" "
+                                    sh "ssh  ${server3} \"${preparecommand}\""
                                 }
                             }
                         },
                         "Load4": {
                             script {
                                 if (server4 != "OFF") {
-                                    sh "ssh  ${server4} \"rm -rf ./perf && mkdir perf && mkdir perf/results && unzip perf.zip -d perf \" "
+                                    sh "ssh  ${server4} \"${preparecommand}\""
                                 }
                             }
                         }
@@ -101,33 +107,55 @@ stage('Prepare loaders') {
                         "Load1": {
                             script {
                                 if (server1 != "OFF") {
-                                    sh "ssh  ${server1} \"export _JAVA_OPTIONS='-Xms3072m -Xmx5120m' && cd perf && ./apache-jmeter-5.3/bin/jmeter.sh -n -t ~/perf/scripts/${PERF_TEST}.jmx -l ~/perf/results/${PERF_TEST}/${BUILD_NUMBER}.jtl\" "
+                                    sh "ssh  ${server1} \"${startcommand}\""
                                 }
                             }
                         },
                         "Load2": {
                             script {
                                 if (server2 != "OFF") {
-                                    sh "ssh  ${server2} \"export _JAVA_OPTIONS='-Xms3072m -Xmx5120m' && cd perf && ./apache-jmeter-5.3/bin/jmeter.sh -n -t ~/perf/scripts/${PERF_TEST}.jmx -l ~/perf/results/${PERF_TEST}/${BUILD_NUMBER}.jtl\" "
+                                    sh "ssh  ${server2} \"${startcommand}\""
                                 }
                             }
                         },
                         "Load3": {
                             script {
                                 if (server3 != "OFF") {
-                                    sh "ssh  ${server3} \"export _JAVA_OPTIONS='-Xms3072m -Xmx5120m' && cd perf && ./apache-jmeter-5.3/bin/jmeter.sh -n -t ~/perf/scripts/${PERF_TEST}.jmx -l ~/perf/results/${PERF_TEST}/${BUILD_NUMBER}.jtl\" "
+                                    sh "ssh  ${server3} \"${startcommand}\""
                                 }
                             }
                         },
                         "Load4": {
                             script {
                                 if (server4 != "OFF") {
-                                    sh "ssh  ${server4} \"export _JAVA_OPTIONS='-Xms3072m -Xmx5120m' && cd perf && ./apache-jmeter-5.3/bin/jmeter.sh -n -t ~/perf/scripts/${PERF_TEST}.jmx -l ~/perf/results/${PERF_TEST}/${BUILD_NUMBER}.jtl\" "
+                                    sh "ssh  ${server4} \"${startcommand}\""
                                 }
                             }
                         }
               ) 
         }
-      }
+    
+      post {
+        always {
+                            script {
+                                if (server1 != "OFF") {
+                                    sh "ssh  ${server1} \"${killcommand}\""
+                                }
+                            
+                                if (server2 != "OFF") {
+                                    sh "ssh  ${server2} \"${killcommand}\""
+                                }
+                        
+                                if (server3 != "OFF") {
+                                    sh "ssh  ${server3} \"${killcommand}\""
+                                }
+                        
+                                if (server4 != "OFF") {
+                                    sh "ssh  ${server4} \"${killcommand}\""
+                                }
+                            }
+                        } 
+        }
+    }
   } 
 }
